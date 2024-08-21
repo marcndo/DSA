5c07c331d7790fe834cd34c137f664765fa13e80
Marcel Ndowah <ndowahmarcel@gmail.com>
2024-08-21 18:27:33 +0100

    redesign add_bin_int algorithm

--- DSA_MIT/binary_operations/add_bin_int.py
+++ DSA_MIT/binary_operations/add_bin_int.py
@@ -1,47 +1,23 @@
 """
-add binary integers Pseudo code
-===============================
-constraint: both arrays are of same length
-ADD-BINARY-INTEGERS(l1, l2, n)
-    sub_total = []
-    add = 0
-    l1 = l1[::-1]
-    l2 = l2[::-1]
-    for i in l1:
-      for j in l2:
-      if i == j
-          add = l1[i] + l2[j]
-          if add < 2
-              sub_total.insert(add)
-          else
-              while i < n-1 and j < n-1
-                  mod = (l1[i]+l[j]) % 2
-                  quotient = (l1[i]+l[j])//2
-                  sub_total.insert(mod)
-                  add += quotient
-              sub_total.insert(add%2)
-              sub_total.insert(add//2)
-    return sub_total    
+ADD-BINARY-INTEGER(A,B, n)
+    subtotal = []
+    carry = 0
+    for i = 0 to n
+#access elements in A&B in reverse order.
+       current_sum = A[i] + B[i] + carry
+       sub_total.insert(0, current_sum%2)
+       carry = current_sum//2
+    if carry
+        sub_total.insert(0, carry)
 """
-def add_binary_integer(arr1, arr2, n):
+def add_binary_integer(arr, arr1, n):
     sub_total = []
-    add = 0
-    arr1 = arr1[::-1]
-    arr2 = arr2[::-1]
-    for i in range(len(arr1)):
-        for j in range(len(arr2)):
-            if i == j:
-                add = arr1[i] + arr2[j]
-                if add < 2:
-                    sub_total.insert(0, add)
-                else:
-                    while i < n-1:
-                        mod = (arr1[i] + arr2[j])%2
-                        quotient = (arr1[i] + arr2[j])//2
-                        sub_total.insert(0, mod)
-                        add +=quotient
-                    sub_total.insert(0,add%2)
-                    sub_total.insert(0,add//2)
+    carry = 0
+    for i in range(n-1, -1, -1):
+        current_sum = arr[i] + arr1[i] + carry
+        sub_total.insert(0, current_sum%2)
+        carry = current_sum//2
+    if carry:
+        sub_total.insert(0, carry)
     return sub_total
 
-print(add_binary_integer([1, 0], [1, 1], 3))
