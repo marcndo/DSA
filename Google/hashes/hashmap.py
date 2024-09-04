5bc3096a07b4453f4cb9b746647c79a2f11dc3e1
Marcel Ndowah <ndowahmarcel@gmail.com>
2024-09-04 08:19:52 +0100

    fix put function to insert elements

--- Google/hashes/hashmap.py
+++ Google/hashes/hashmap.py
@@ -1,16 +1,19 @@
 class MyHashMap:
 
-    def __init__(self, ):
+    def __init__(self):
         self.map_ = []
-
+#needs to fix the put function to avoid duplicate values in line 12 through 16
     def put(self, key: int, value: int) -> None:
         self.key = key
         self.value = value
-        for item in self.map_:
-            if self.key == item[0]:
-                item[1] = self.value
-            else:
-               self.map_.append([self.key, self.value])
+        if len(self.map_) == 0:
+            self.map_.append([key, value])
+        else:
+            for item in self.map_:
+                if item[0] == key:
+                    item[1] = value
+                elif [self.key, self.value] not in self.map_:
+                    self.map_.append([self.key, self.value])
 
     def get(self, key: int) -> int:
         return self.key
@@ -27,4 +30,5 @@ class MyHashMap:
 myHashMap = MyHashMap()
 myHashMap.put(1, 1)
 myHashMap.put(2, 2)
+myHashMap.put(2, 8)
 print(myHashMap.map_)
