224ad0b408c2fc50853509e149bd2482bc93dc84
Marcel Ndowah <ndowahmarcel@gmail.com>
2024-09-16 09:36:07 +0100

    update hashset.py with hash function

--- Google/hashes/hashset.py
+++ Google/hashes/hashset.py
@@ -1,20 +1,22 @@
 class MyHashSet:
-
     def __init__(self):
         self.item = []
 
+    def _hash_function(self, key):
+        return hash(key)
+
     def add(self, key: int) -> None:
         self.key = key
-        if self.key in self.item:
-            pass
+        if self._hash_function(key) in self.item:
+            self.key = self._hash_function(key)
         else:
-            self.item.append(self.key)
+            self.item.append(self._hash_function(key))
 
     def remove(self, key: int) -> None:
-        self.item.remove(self.key)
+        self.item.remove(self._hash_function(key))
 
     def contains(self, key: int) -> bool:
-        if self.key in self.item:
+        if self._hash_function(key) in self.item:
             return True
         else:
             return False
@@ -25,7 +27,10 @@ object1 = MyHashSet()
 object1.add(1)
 object1.add(2)
 print(object1.contains(1))
-print(object1.item)
-print(object1.remove(2))
-print(object1.item)
 print(object1.contains(3))
+object1.add(2)
+print(object1.item)
+print(object1.contains(2))
+print(object1.remove(2))
+print(object1.contains(2))
+
